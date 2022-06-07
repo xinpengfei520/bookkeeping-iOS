14d90b6e4a25355c388edccd9106032e582a3028
Xin Vance <xinpengfei520@gmail.com>
2022-06-07 16:53:15 +0800

    [feature][搜索]: 优化搜索框逻辑

--- bookkeeping/bookkeeping/Classes/Modules/Search (搜索)/View/SearchNavigation.m
+++ bookkeeping/bookkeeping/Classes/Modules/Search (搜索)/View/SearchNavigation.m
@@ -8,16 +8,29 @@
 
 #import "SearchNavigation.h"
 
+@interface SearchNavigation ()<UITextFieldDelegate>
+
+@end
+
 @implementation SearchNavigation
 
 - (void)initUI {
-    [self setBackgroundColor:kColor_Main_Color];
-    self.searchTextField.placeholder = @"类别/备注/金额";
-    
-    UIImageView *imageView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"icon_search_gray.png"]];
-    [self.searchTextField setLeftView:imageView];
-    [self.searchTextField setLeftViewMode:UITextFieldViewModeAlways];
+	[self setBackgroundColor:kColor_Main_Color];
+	self.searchTextField.placeholder = @"类别/备注/金额";
 
+	UIImageView *imageView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"icon_search_gray.png"]];
+	[self.searchTextField setLeftView:imageView];
+	[self.searchTextField setLeftViewMode:UITextFieldViewModeAlways];
+	[self.searchTextField setClearButtonMode:UITextFieldViewModeWhileEditing];
+	_searchTextField.returnKeyType = UIReturnKeySearch;//变为搜索按钮
+	_searchTextField.delegate = self;//设置代理
 }
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+	NSLog(@"点击了搜索");
+    [_searchTextField isFirstResponder:NO]
+	return YES;
+}
+
 
 @end
