cecccfc37711cb4358ab285269d2220b7dfca1e3
Xin Vance <xinpengfei520@gmail.com>
2022-07-08 17:48:58 +0800

    账本添加功能

--- bookkeeping/bookkeeping/Classes/Modules/Books(账本)/BooksController.m
+++ bookkeeping/bookkeeping/Classes/Modules/Books(账本)/BooksController.m
@@ -7,8 +7,14 @@
 //
 
 #import "BooksController.h"
+#import "BottomButton.h"
+#import "AlertViewManager.h"
+#import "CA_EVENT.h"
 
 @interface BooksController ()
+
+@property (nonatomic, strong) BottomButton *bottom;
+@property (nonatomic, strong) NSDictionary<NSString *, NSInvocation *> *eventStrategy;
 
 @end
 
@@ -19,6 +25,56 @@
     self.hbd_barHidden = NO;
     self.hbd_barTintColor = kColor_Main_Color;
     [self setNavTitle:@"账本"];
+    [self bottom];
+}
+
+- (BottomButton *)bottom {
+    if (!_bottom) {
+        _bottom = [BottomButton initWithFrame:({
+            CGFloat height = countcoordinatesX(50) + SafeAreaBottomHeight;
+            CGFloat top = SCREEN_HEIGHT - height - NavigationBarHeight;
+            CGRectMake(0, top, SCREEN_WIDTH, height);
+        })];
+        [_bottom setName:@"创建账本"];
+        [self.view addSubview:_bottom];
+    }
+    _bottom.layer.shadowPath = [UIBezierPath bezierPathWithRect:_bottom.bounds].CGPath;
+    return _bottom;
+}
+
+- (void)bottomClick:(id)data {
+    [[AlertViewManager sharedInstacne]showSheet:nil message:nil cancelTitle:@"取消" viewController:self confirm:^(NSInteger buttonTag,NSString *buttonTitle) {
+        [self createBook:buttonTag];
+    } buttonTitles:@"生意账本", @"报销账本", @"公司账本", @"团队账本", @"自定义账本", nil];
+}
+
+- (void)createBook:(NSInteger)tag{
+    if (tag == 0) {
+        
+    } else if (tag == 1) {
+        
+    }
+}
+
+#pragma mark - 事件
+- (void)routerEventWithName:(NSString *)eventName data:(id)data {
+    [self handleEventWithName:eventName data:data];
+}
+
+- (void)handleEventWithName:(NSString *)eventName data:(id)data {
+    NSInvocation *invocation = self.eventStrategy[eventName];
+    [invocation setArgument:&data atIndex:2];
+    [invocation invoke];
+    [super routerEventWithName:eventName data:data];
+}
+
+- (NSDictionary<NSString *, NSInvocation *> *)eventStrategy {
+    if (!_eventStrategy) {
+        _eventStrategy = @{
+            CATEGORY_BTN_CLICK: [self createInvocationWithSelector:@selector(bottomClick:)],
+        };
+    }
+    return _eventStrategy;
 }
 
 
