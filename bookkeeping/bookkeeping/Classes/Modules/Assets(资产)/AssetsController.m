9ffdd588946a51f287a45cb74b21e653f7a22440
Xin Vance <xinpengfei520@gmail.com>
2022-07-08 18:29:59 +0800

    添加资产

--- bookkeeping/bookkeeping/Classes/Modules/Assets(资产)/AssetsController.m
+++ bookkeeping/bookkeeping/Classes/Modules/Assets(资产)/AssetsController.m
@@ -7,9 +7,13 @@
 //
 
 #import "AssetsController.h"
-
+#import "BottomButton.h"
+#import "CA_EVENT.h"
 
 @interface AssetsController ()
+
+@property (nonatomic, strong) BottomButton *bottom;
+@property (nonatomic, strong) NSDictionary<NSString *, NSInvocation *> *eventStrategy;
 
 @end
 
@@ -20,7 +24,46 @@
     self.hbd_barHidden = NO;
     self.hbd_barTintColor = kColor_Main_Color;
     [self setNavTitle:@"资产"];
+    [self bottom];
 }
 
+- (BottomButton *)bottom {
+    if (!_bottom) {
+        _bottom = [BottomButton initWithFrame:({
+            CGFloat height = countcoordinatesX(50) + SafeAreaBottomHeight;
+            CGFloat top = SCREEN_HEIGHT - height - NavigationBarHeight;
+            CGRectMake(0, top, SCREEN_WIDTH, height);
+        })];
+        [_bottom setName:@"+添加资产"];
+        [self.view addSubview:_bottom];
+    }
+    _bottom.layer.shadowPath = [UIBezierPath bezierPathWithRect:_bottom.bounds].CGPath;
+    return _bottom;
+}
+
+- (void)bottomClick:(id)data {
+    
+}
+
+#pragma mark - 事件
+- (void)routerEventWithName:(NSString *)eventName data:(id)data {
+    [self handleEventWithName:eventName data:data];
+}
+
+- (void)handleEventWithName:(NSString *)eventName data:(id)data {
+    NSInvocation *invocation = self.eventStrategy[eventName];
+    [invocation setArgument:&data atIndex:2];
+    [invocation invoke];
+    [super routerEventWithName:eventName data:data];
+}
+
+- (NSDictionary<NSString *, NSInvocation *> *)eventStrategy {
+    if (!_eventStrategy) {
+        _eventStrategy = @{
+            CATEGORY_BTN_CLICK: [self createInvocationWithSelector:@selector(bottomClick:)],
+        };
+    }
+    return _eventStrategy;
+}
 
 @end
