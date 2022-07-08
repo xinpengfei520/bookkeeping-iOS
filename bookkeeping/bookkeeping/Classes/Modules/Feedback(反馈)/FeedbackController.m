7bf97a0bcf42a01adc454224013a67d5c883e3e3
Xin Vance <xinpengfei520@gmail.com>
2022-07-08 18:26:41 +0800

    反馈功能

--- bookkeeping/bookkeeping/Classes/Modules/Feedback(反馈)/FeedbackController.m
+++ bookkeeping/bookkeeping/Classes/Modules/Feedback(反馈)/FeedbackController.m
@@ -8,7 +8,9 @@
 
 #import "FeedbackController.h"
 
-@interface FeedbackController ()
+@interface FeedbackController ()<UITextFieldDelegate>
+
+@property (strong, nonatomic) IBOutlet UITextField *textField;
 
 @end
 
@@ -19,7 +21,30 @@
     self.hbd_barHidden = NO;
     self.hbd_barTintColor = kColor_Main_Color;
     [self setNavTitle:@"反馈"];
+    [self textField];
+    [self.textField becomeFirstResponder];
 }
 
+- (UITextField *)textField {
+    if (!_textField) {
+        CGFloat padding = 16;
+        _textField = [[UITextField alloc]initWithFrame:CGRectMake(padding,0,SCREEN_WIDTH-padding*2, countcoordinatesX(64))];
+        _textField.backgroundColor = [UIColor whiteColor];
+        _textField.placeholder = @"请输入您的问题";
+        [self.textField setClearButtonMode:UITextFieldViewModeWhileEditing];
+        // 变为发送按钮
+        _textField.returnKeyType = UIReturnKeySend;
+        // 设置代理
+        _textField.delegate = self;
+        [self.view addSubview:_textField];
+    }
+    return _textField;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [_textField resignFirstResponder];
+    //[self routerEventWithName:SEARCH_TEXT_INPUT data:textField.text];
+    return YES;
+}
 
 @end
