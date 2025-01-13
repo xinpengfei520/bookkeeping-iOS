4619824083230b7debb26e5c1f3450b538916ef8
Xin Vance <xinpengfei520@gmail.com>
2025-01-13 18:50:36 +0800

    update

--- bookkeeping/bookkeeping/Classes/Modules/About/Controller/AboutController.m
+++ bookkeeping/bookkeeping/Classes/Modules/About/Controller/AboutController.m
@@ -44,14 +44,16 @@
     // App名称（动态获取）
     _titleLabel = [[UILabel alloc] init];
     NSDictionary *infoDictionary = [[NSBundle mainBundle] infoDictionary];
-    _titleLabel.text = [infoDictionary objectForKey:@"CFBundleDisplayName"] ?: @"记账";
+    _titleLabel.text = [infoDictionary objectForKey:@"CFBundleDisplayName"] ?: @"记呀";
     _titleLabel.font = [UIFont systemFontOfSize:24 weight:UIFontWeightMedium];
     _titleLabel.textAlignment = NSTextAlignmentCenter;
     [self.view addSubview:_titleLabel];
     
     // 版本号
     _versionLabel = [[UILabel alloc] init];
-    _versionLabel.text = [NSString stringWithFormat:@"V%@", [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"]];
+    NSString *version = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleShortVersionString"];
+    NSString *build = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
+    _versionLabel.text = [NSString stringWithFormat:@"V%@(%@)", version, build];
     _versionLabel.font = [UIFont systemFontOfSize:AdjustFont(14)];
     _versionLabel.textColor = kColor_Text_Gary;
     _versionLabel.textAlignment = NSTextAlignmentCenter;
@@ -90,11 +92,17 @@
         make.top.equalTo(self.view).offset(NavigationBarHeight + 20);
         make.width.height.equalTo(@100);
     }];
+
+    // App名称
+    [_titleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.view);
+        make.top.equalTo(_iconView.mas_bottom).offset(10);
+    }];
     
     // 版本号
     [_versionLabel mas_makeConstraints:^(MASConstraintMaker *make) {
         make.centerX.equalTo(self.view);
-        make.top.equalTo(_iconView.mas_bottom).offset(10);
+        make.top.equalTo(_titleLabel.mas_bottom).offset(10);
     }];
     
     // 用户协议按钮
@@ -114,7 +122,7 @@
     // 备案号
     [_icpLabel mas_makeConstraints:^(MASConstraintMaker *make) {
         make.centerX.equalTo(self.view);
-        make.bottom.equalTo(self.view).offset(-20);
+        make.bottom.equalTo(self.view).offset(-40);
     }];
 }
 
