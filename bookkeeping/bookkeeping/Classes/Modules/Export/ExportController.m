3d35dc9706055b789a5cede530938838108e92ff
Xin Vance <xinpengfei520@gmail.com>
2025-03-30 22:52:37 +0800

    feature: export to file.

--- bookkeeping/bookkeeping/Classes/Modules/Export/ExportController.m
+++ bookkeeping/bookkeeping/Classes/Modules/Export/ExportController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ExportController.h"
+#import <Masonry/Masonry.h>
 
 @interface ExportController ()
 
@@ -18,9 +19,100 @@
     [super viewDidLoad];
     self.hbd_barHidden = NO;
     self.hbd_barTintColor = kColor_Main_Color;
-    [self setNavTitle:@"导出"];
+    [self setNavTitle:@"导出数据"];
+    
+    [self setupUI];
 }
 
+- (void)setupUI {
+    // 创建导出按钮
+    UIButton *exportButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [exportButton setTitle:@"点击导出数据到文件" forState:UIControlStateNormal];
+    [exportButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [exportButton setBackgroundColor:kColor_Main_Color];
+    [exportButton.layer setCornerRadius:5.0];
+    [exportButton addTarget:self action:@selector(exportToCSV) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:exportButton];
+    
+    // 设置布局
+    [exportButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.view);
+        make.centerY.equalTo(self.view);
+        make.width.equalTo(@200);
+        make.height.equalTo(@44);
+    }];
+}
 
+- (void)exportToCSV {
+    // 1. 获取所有记账数据
+    NSMutableArray<BookDetailModel *> *allRecords = [NSUserDefaults getAllBookList];
+    
+    // 2. 创建CSV内容头部
+    NSMutableString *csvString = [NSMutableString stringWithString:@"日期,类别,金额,收支类型,备注\n"];
+    
+    // 3. 将数据转换为CSV格式
+    for (BookDetailModel *model in allRecords) {
+        NSString *date = [NSDate stringWithDate:model.date format:@"yyyy-MM-dd"];
+        
+        // 通过categoryId获取类别
+        BKCModel *categoryModel = [NSUserDefaults getCategoryModel:model.categoryId];
+        NSString *category = categoryModel.name;
+        
+        // 根据金额值判断是收入还是支出
+        NSString *amount;
+        NSString *type;
+        
+        if (model.categoryId >= 33) {
+            amount = [NSString stringWithFormat:@"%.2f", model.price];
+            type = @"收入";
+        } else {
+            amount = [NSString stringWithFormat:@"%.2f", model.price];
+            type = @"支出";
+        }
+        
+        NSString *remark = model.mark ?: @"";
+        
+        // 处理备注中的逗号，用引号包裹防止CSV解析错误
+        if ([remark containsString:@","]) {
+            remark = [NSString stringWithFormat:@"\"%@\"", remark];
+        }
+        
+        [csvString appendFormat:@"%@,%@,%@,%@,%@\n", date, category, amount, type, remark];
+    }
+    
+    // 4. 保存文件到Documents目录
+    NSString *docPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES).firstObject;
+    NSString *fileName = [NSString stringWithFormat:@"记账导出_%@.csv", [self getCurrentTimeString]];
+    NSString *filePath = [docPath stringByAppendingPathComponent:fileName];
+    
+    NSError *error;
+    [csvString writeToFile:filePath atomically:YES encoding:NSUTF8StringEncoding error:&error];
+    
+    if (error) {
+        [self showTextHUD:@"导出失败" delay:1.5f];
+        return;
+    }
+    
+    // 5. 分享文件
+    [self shareFileAtPath:filePath];
+}
+
+// 获取当前时间字符串，用于文件名
+- (NSString *)getCurrentTimeString {
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"yyyyMMdd_HHmmss"];
+    return [formatter stringFromDate:[NSDate date]];
+}
+
+// 分享文件
+- (void)shareFileAtPath:(NSString *)filePath {
+    NSURL *fileURL = [NSURL fileURLWithPath:filePath];
+    UIActivityViewController *activityVC = [[UIActivityViewController alloc] 
+                                           initWithActivityItems:@[fileURL] 
+                                           applicationActivities:nil];
+    
+    [self presentViewController:activityVC animated:YES completion:nil];
+}
 
 @end
